ca59f450f44cfd118df564e6587317fda5fdf54e
DanWttvn <dan.wttvn@gmail.com>
2020-05-13 17:35:54 +0200

    sections organization changes

--- js/main.js
+++ js/main.js
@@ -238,3 +238,28 @@ function closeCard() {
 	cardSp.style.display = "none";
 	cardTz.style.display = "none";
 }
+
+
+
+/* <---------- Accordion ----------> */
+const accordionBtns = document.querySelectorAll(".accordion-btn");
+
+// First open by default
+const firstAccContent = document.querySelector(".accordion-btn.open + .accordion-content");
+firstAccContent.style.maxHeight = firstAccContent.scrollHeight + "px"
+
+accordionBtns.forEach(accBtn => {
+	accBtn.addEventListener("click", function() {
+		// accordionBtns.forEach(accBtn => accBtn.classList.remove("open")) se me borra en todos, asi que no vale
+
+		accBtn.classList.toggle("open");
+		
+		const content = accBtn.nextElementSibling;
+		if(content.style.maxHeight) {
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px"
+		}
+	})
+})
+
--- js/payments.js
+++ js/payments.js
@@ -1,30 +1,58 @@
 
+/* <---------- Stripe ----------> */
+(function() {
+	var stripe = Stripe('pk_test_JX0416brSyfuCM9j6ZCVsorX00xR15bINY');
 
-/* <---------- Accordion ----------> */
+	var checkoutButton = document.getElementById('checkout-button-sku_HF2Zqg8Ayxqr6x');
 
-const accordionBtns = document.querySelectorAll(".accordion-btn");
+	checkoutButton.addEventListener('click', function () {
+		// When the customer clicks on the button, redirect
+		// them to Checkout.
+		stripe.redirectToCheckout({
+			items: [{sku: 'sku_HF2Zqg8Ayxqr6x', quantity: 1}],
+			
+			// Do not rely on the redirect to the successUrl for fulfilling
+			// purchases, customers may not always reach the success_url after
+			// a successful payment.
+			// Instead use one of the strategies described in
+			// https://stripe.com/docs/payments/checkout/fulfillment
+			successUrl: 'http://localhost:5500/donate-success.html',
+			cancelUrl: 'http://localhost:5500/donate.html',
+		})
+		.then(function (result) {
+			if (result.error) {
+				// If `redirectToCheckout` fails due to a browser or network
+				// error, display the localized error message to your customer.
+				var displayError = document.getElementById('error-message');
+				displayError.textContent = result.error.message;
+			}
+		});
+	});
 
-// First open by default
-const firstAccContent = document.querySelector(".accordion-btn.open + .accordion-content");
-firstAccContent.style.maxHeight = firstAccContent.scrollHeight + "px"
+	var stripeSubscription = Stripe('pk_test_U1vwYDpxJPMhNaupWvnihtOL00HTg9c4J9');
 
-accordionBtns.forEach(accBtn => {
-	accBtn.addEventListener("click", function() {
-		accBtn.style
-
-		// accordionBtns.forEach(accBtn => accBtn.classList.remove("open")) se me borra en todos, asi que no vale
-
-		accBtn.classList.toggle("open");
+	var checkoutButton2 = document.getElementById('checkout-button-plan_HF4CfR984Uf4i4');
+	checkoutButton2.addEventListener('click', function () {
+		// When the customer clicks on the button, redirect
+		// them to Checkout.
+		stripeSubscription.redirectToCheckout({
+			items: [{plan: 'plan_HF4CfR984Uf4i4', quantity: 1}],
 		
-		const content = accBtn.nextElementSibling;
-		if(content.style.maxHeight) {
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px"
-		}
-	})
-})
-
-
-
-/* <---------- Menu abrir ----------> */
+			// Do not rely on the redirect to the successUrl for fulfilling
+			// purchases, customers may not always reach the success_url after
+			// a successful payment.
+			// Instead use one of the strategies described in
+			// https://stripe.com/docs/payments/checkout/fulfillment
+			successUrl: 'http://localhost:5500/donate-success.html',
+			cancelUrl: 'http://localhost:5500/donate.html'
+		})
+		.then(function (result) {
+			if (result.error) {
+				// If `redirectToCheckout` fails due to a browser or network
+				// error, display the localized error message to your customer.
+				var displayError = document.getElementById('error-message');
+				displayError.textContent = result.error.message;
+			}
+		});
+	});
+})();
